8a2395a1590d6b337de7aa1e6ee19211931d943d
zachagarrett <zach.garrett.archery@gmail.com>
2023-10-17 11:21:19 -0500

    update copy

--- src/app/components/copy.ts
+++ src/app/components/copy.ts
@@ -2,17 +2,17 @@ export const copy = {
   testA: {
     title: "FutureZero - Increase your home's value",
     prompt:
-      "How much does your home value increase by making it net zero? Add your information to receive an estimate.",
+      "How much would your home value increase from a clean energy retrofit?",
   },
   testB: {
     title: "FutureZero - Reduce your home's carbon impact",
     prompt:
-      "Homes add 40 tons of CO2 on average ever year. Enter your home information to find out how much of your home's carbon you can keep out of the atmosphere. ",
+      "How much could you reduce your home's C02 emissions with a clean energy retrofit?",
   },
   howItWorks:
-    "Add your email and the home address you want analyzed. Our expert team will crunch the numbers and share the results with you over email.",
+    "Simply input your home address and your email address to get sent a free home health and valuation estimate.",
   aboutUs:
-    "FutureZero is a start up that leverages deep real estate expertise and artificial intelligence to help families make their homes safer, more valuable and friendly for the planet.",
+    "FutureZero is a real estate technology startup that helps homeowners increase the health and value of their homes through net zero energy upgrades.",
   components: {
     intakeForm: {
       fields: {
